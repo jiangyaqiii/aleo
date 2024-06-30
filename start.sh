72a07ff6715bf34ba651aaa08000068456fd5a2e
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-30 23:03:54 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,5 +15,7 @@ function install_pm2() {
 echo '正在按照pm2'
 install_pm2
 echo '正在下载aleo-pool-prover客户端'
+read -p "请输入accountname: " accountname
+read -p "请输入mechaname: " mechaname
 wget -O aleo-pool-prover https://github.com/zkrush/aleo-pool-client/releases/download/v1.5-testnet-beta/aleo-pool-prover && chmod +x aleo-pool-prover
-pm2 start ./aleo-pool-prover --name "aleo-pool-prover" -- --pool wss://aleo.zkrush.com:3333 --account accountname --worker-name mechaname
+pm2 start ./aleo-pool-prover --name "aleo-pool-prover" -- --pool wss://aleo.zkrush.com:3333 --account $accountname --worker-name $mechaname
