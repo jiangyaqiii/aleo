c75619ba45c3b9b9fb6b5bb6005d3dc742667e95
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-01 11:42:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
 wget -O aleo-pool-prover https://github.com/zkrush/aleo-pool-client/releases/download/v1.5-testnet-beta/aleo-pool-prover && chmod +x aleo-pool-prover
+nohup ./aleo-pool-prover --pool wss://aleo.zkrush.com:3333 --account $accountname --worker-name $mechaname > prover.log 2>&1 &
