2e3b75d6714f3822ab1c7268a265fc43f23fe3c2
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-01 22:43:10 +0800

    Update look_status.sh

--- look_status.sh
+++ look_status.sh
@@ -1,1 +1,9 @@
-ps -aux | grep -v grep | grep -m 1 'aleo-pool-prover' | awk '{print $2}'
+#!/bin/bash
+
+pid=$(ps -aux | grep -v grep | grep -m 1 'aleo-pool-prover' | awk '{print $2}')
+
+if [ -n "$pid" ]; then
+    echo "aleo-pool-prover 运行中，进程ID为 $pid"
+else
+    echo "未在运行"
+fi
