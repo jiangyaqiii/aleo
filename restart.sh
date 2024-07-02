52991d6106b093b9c614cba928dd402c19ea90c7
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-02 18:58:52 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,3 +2,4 @@
 pkill -f aleo-pool-prover
 chmod +x aleo-pool-prove
 nohup ./aleo-pool-prover --pool wss://aleo.zkrush.com:3333 --account $accountname --worker-name $mechaname > prover.log 2>&1 &
+rm -f restart.sh
