8499835c7f8072a75b4d0e2c71d8df04ed78496e
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-03 22:33:04 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,25 @@
 pkill -f aleo-pool-prove
-if [ ! -f "aleo-pool-prove" ]; then
-    echo "aleo-pool-prove 文件不存在"
-    echo "开始下载并设置aleo-pool-prove..."
+if [ ! -f "aleo-pool-prover" ]; then
+    echo "aleo-pool-prover 文件不存在"
+    echo "开始下载并设置aleo-pool-prover..."
 
-    wget -O aleo-pool-prove https://github.com/zkrush/aleo-pool-client/releases/download/v1.6-testnet-beta/aleo-pool-prover && chmod +x aleo-pool-prove
+    wget -O aleo-pool-prover https://github.com/zkrush/aleo-pool-client/releases/download/v1.6-testnet-beta/aleo-pool-prover && chmod +x aleo-pool-prover
 
     if [ $? -eq 0 ]; then
-        chmod +x aleo-pool-prove
-        echo "aleo-pool-prove 下载并设置成功"
+        chmod +x aleo-pool-prover
+        echo "aleo-pool-prover 下载并设置成功"
     else
-        echo "下载或设置aleo-pool-prove时出现错误"
+        echo "下载或设置aleo-pool-prover时出现错误"
     fi
 else
-    echo "aleo-pool-prove 文件已经存在,删除原版本，下载新版本"
-    rm -rf aleo-pool-prove
-    wget -O aleo-pool-prove https://github.com/zkrush/aleo-pool-client/releases/download/v1.6-testnet-beta/aleo-pool-prover && chmod +x aleo-pool-prove
+    echo "aleo-pool-prover 文件已经存在,删除原版本，下载新版本"
+    rm -rf aleo-pool-prover
+    wget -O aleo-pool-prover https://github.com/zkrush/aleo-pool-client/releases/download/v1.6-testnet-beta/aleo-pool-prover && chmod +x aleo-pool-prover
     if [ $? -eq 0 ]; then
-        chmod +x aleo-pool-prove
-        echo "aleo-pool-prove 下载并设置成功"
+        chmod +x aleo-pool-prover
+        echo "aleo-pool-prover 下载并设置成功"
     else
-        echo "下载或设置aleo-pool-prove时出现错误"
+        echo "下载或设置aleo-pool-prover时出现错误"
     fi
 fi
 nohup ./aleo-pool-prover --pool wss://aleo.zkrush.com:3333 --account $accountname --worker-name $mechaname > prover.log 2>&1 &
