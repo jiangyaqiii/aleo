7b2d4b44f6569dd1a8d770b3e3e77509a997efd1
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-05 11:33:22 +0800

    Create gpu_restart.sh

---
+++ gpu_restart.sh
@@ -0,0 +1,4 @@
+# pid ps -aux | grep -v grep | grep -m 1 'aleo-pool-prover' | awk '{print $2}'
+pkill -f aleo-miner
+nohup ./aleo-miner -u stratum+ssl://nlb-gnbhhkqxxdgt9wc6ou.cn-chengdu.nlb.aliyuncs.com:4420 -w $accountname.$mechaname -d 0 >> ./aleo-miner.log 2>&1 &
+rm -f gpu_restart.sh
