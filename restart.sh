c0aaf91eadcc9d6abf92528f182a7988617d7521
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-07-05 11:26:37 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 # pid ps -aux | grep -v grep | grep -m 1 'aleo-pool-prover' | awk '{print $2}'
 pkill -f aleo-pool-prove
-chmod +x aleo-pool-prove
-nohup ./aleo-pool-prove --pool wss://aleo.zkrush.com:3333 --account $accountname --worker-name $mechaname > prover.log 2>&1 &
+pkill -f aleo-pool-prover
+nohup ./aleo-pool-prover --pool wss://aleo.zkrush.com:3333 --account $accountname --worker-name $mechaname > prover.log 2>&1 &
 rm -f restart.sh
